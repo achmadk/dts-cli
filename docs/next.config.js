08375403f6c37a4669590de2155e41665bd02e3b
sidwebworks <xodedeveloper@gmail.com>
2021-10-12 20:33:03 +0530

    fix: Add `.nojekyll` file in doc output dir

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,7 +1,10 @@
-const compose = require("compose-function");
-const { withDokz } = require("dokz/dist/plugin");
+const compose = require('compose-function');
+const { withDokz } = require('dokz/dist/plugin');
 const composed = compose(withDokz);
 
+const debug = process.env.NODE_ENV !== 'production';
+
 module.exports = composed({
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  assetPrefix: !debug ? 'https://weiran-zsd.github.io/dts-cli/' : '',
 });
